7a19cc23f47245e76ba9fd39ecb1157854b9cc0a
Dinh Diem <dinhdiem531@gmail.com>
2024-08-20 22:33:03 +0700

    add home.php file

---
+++ home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<HTML LANG="EN">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Document</title>
+    
+</head>
+<BODY>
+    <?php
+    ECHO '<center><img  width="20%" src="https://inkythuatso.com/uploads/thumbnails/800/2021/12/logo-dai-hoc-thuong-mai-inkythuatso-01-23-09-46-38.jpg"  </center><br>';
+    ECHO '<h3 align="center"> Form điền thông tin tân sinh viên </h3>';
+    
+    ECHO'
+    <form name=" Điền thông tin" method="post" action="#">
+       
+       
+        <table align="center">
+            <tr>
+                <td class="tdLabel"><label for="register_name" class="label">Họ và tên:</label></td>
+                <td><input type="text" name="name" id="register_name" style="width:160px ; border-color:#003399 ; border-style:solid"/></td>
+            </tr>
+            <tr>
+                <td class="tdLabel"><label for="register_HBD" class="label">Ngày sinh:</label></td>
+                <td><input type="date" name="HBD" id="register_HBD" style="width:160px; border-color:#003399 ; border-style:solid"/></td>
+            </tr>
+            <tr>
+                <td class="tdLabel"><label for="gender" class="label">Giới tính:</label></td>
+                <td>
+                    <input type="radio" name="gender" id="gender1"/>
+                        <label for="gender1">Nam</label>
+                    <input type="radio" name="gender" id="gender2" />
+                        <label for="gender2">Nữ</label>
+                    <input type="radio" name="gender" id="gender3" />
+                        <label for="gender3">Khác</label>
+                </td>
+            </tr>
+            <tr>
+                <td class="tdLabel"><label for="sdt" class="label">SĐT:</label></td>
+                <td><input type="text" name="sdt" id="sdt" style="width:160px; border-color:#003399 ; border-style:solid"/></td>
+            </tr>
+            <tr>
+                <td class="tdLabel"><label for="register_country" class="label">Quê quán:</label></td>
+                <td><input type="text" name="name" id="register_country" style="width:160px; border-color:#003399 ; border-style:solid"/></td>
+            </tr>
+            <tr align="center">
+                <td>
+                   
+                    <input type="submit" name="btnSubmit" id="btnSubmit" value="Gửi" Style="; border-color:#003399 ; border-style:solid  "/>
+                </td>
+            </tr> 
+            
+        </table>
+        
+        
+    </form>';
+    
+    ECHO'<p align="center"><b>Ghi chú:</b>Nếu có thắc mắc, vui lòng liên hệ qua SĐT: 1112233 hoặc qua trang quản lý đào tạo <a target="_blank" href="https://qldt.tmu.edu.vn/">Tại đây</a></p>';
+    ?>
+
+   
+</BODY>
+
+</HTML>
